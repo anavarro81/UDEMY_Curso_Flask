9b4daac413ab2896fb089b2473e9954b285f5967
anavarro81 <anavarro67@hotmail.com>
2023-02-08 12:02:17 +0100

    Conexion a BBDD

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from my_app.product.views import product
+from sqlalchemy import create_engine, text
+import pymysql
+
+
+
 
 app = Flask(__name__)
 
 # Le pasamos la vista hello_word que hemos importado previamente. 
 app.register_blueprint(product)
+
+
+
+
+
 
 @app.template_filter('mayusculas')
 def convertir_mayusculas(texto):
@@ -13,3 +23,5 @@ def convertir_mayusculas(texto):
 @app.template_filter('mydouble')
 def mydouble_filter(n:int):
     return n*2
+    
+
--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 from my_app import app
+import pymysql
 
 # Ejecuta la apliación.
 # La instancia app de Flash se crea en el archivo __init__.py de my_app
 app.config.from_pyfile("config.py")
 app.run()
 
+conn = pymysql.connect(
+     host="localhost", port=3306, user="root",
+     passwd="", db="pyalmacen"
+ )
+
+cursor = conn.cursor()
+cursor.execute(
+    "SELECT nombre, apellido FROM clientes"
+)
+
+
+for nombre, apellido in cursor.fetchall():
+    print("{0} {1}".format(nombre, apellido))
